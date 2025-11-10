4453b8e2338767effd30ed57e6cf2f8c02f5fa87
byunghyunkim0 <bb05qudgus@gamil.com>
2025-11-10 18:56:47 +0900

    feat: 초기 설정

---
+++ src/test/kotlin/com/my/kotlinlogin/KotlinLoginApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.my.kotlinlogin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinLoginApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
