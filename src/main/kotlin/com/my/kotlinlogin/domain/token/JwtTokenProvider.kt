90684937014fa3af654267459272851cb05f2fec
byunghyunkim0 <bb05qudgus@gamil.com>
2025-11-15 23:40:53 +0900

    feat: AccessToken 생성 기능 구현

---
+++ src/main/kotlin/com/my/kotlinlogin/domain/token/JwtTokenProvider.kt
@@ -0,0 +1,25 @@
+package com.my.kotlinlogin.domain.token
+
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys
+import org.springframework.stereotype.Component
+import java.util.Date
+
+@Component
+class JwtTokenProvider {
+
+    private val secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256)
+
+    fun generateAccessToken(userId: Long): String {
+        val now = Date()
+        val expireDate = Date(now.time + 1000 * 60 * 30)
+
+        return Jwts.builder()
+            .setSubject(userId.toString())
+            .setIssuedAt(now)
+            .setExpiration(expireDate)
+            .signWith(secretKey)
+            .compact()
+    }
+}
