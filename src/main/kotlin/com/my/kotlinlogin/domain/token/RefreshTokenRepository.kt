ca13e3de431a5b8200d585945609eb1c2bd95f7a
byunghyunkim0 <bb05qudgus@gamil.com>
2025-11-20 11:58:26 +0900

    fix: repository 수정

--- src/main/kotlin/com/my/kotlinlogin/domain/token/RefreshTokenRepository.kt
+++ src/main/kotlin/com/my/kotlinlogin/domain/token/RefreshTokenRepository.kt
@@ -1,5 +1,7 @@
 package com.my.kotlinlogin.domain.token
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface RefreshTokenRepository : CrudRepository<RefreshToken, Long>
