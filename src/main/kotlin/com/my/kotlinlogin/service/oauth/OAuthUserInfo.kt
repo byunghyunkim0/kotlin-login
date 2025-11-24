fecb4f0877e1d774908f7b30b12be075e296249d
byunghyunkim0 <bb05qudgus@gamil.com>
2025-11-24 22:47:35 +0900

    fix: 미사용 코드 삭제

--- src/main/kotlin/com/my/kotlinlogin/service/oauth/OAuthUserInfo.kt
+++
@@ -1,8 +0,0 @@
-package com.my.kotlinlogin.service.oauth
-
-interface OAuthUserInfo {
-    fun getProviderId(): String
-    fun getEmail(): String
-    fun getName(): String
-    fun getImageUrl(): String
-}
